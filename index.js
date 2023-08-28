51e7e377134842e77ace977d7bef734c50c14602
phgiang1987 <phgiang1987@gmail.com>
2023-08-28 17:08:58 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express')
 const mongoose = require('mongoose')
-const Book = require("./models/books");
+const Blogs = require("./models/blogs");
 
 const app = express()
 const PORT = process.env.PORT || 3000
